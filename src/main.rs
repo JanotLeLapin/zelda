0f45e1c4a7c1b43411af0b7e05d43becaa7793ee
Joseph DALY <joseph300905@gmail.com>
2024-02-22 00:56:31 +0100

    serve cover

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,15 @@ async fn main() {
         .connect("postgres://postgres:postgres@localhost:5432")
         .await.unwrap();
 
+    for e in std::fs::read_dir("/var/music").unwrap().into_iter().filter_map(|e| e.ok()) {
+        db::scan_album(&pool, e.path()).await.unwrap();
+    }
+
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(pool.clone()))
             .service(web::index::page)
+            .service(web::index::cover)
     })
         .bind(("0.0.0.0", 4000)).unwrap()
         .run().await.unwrap();
--- src/web/index.rs
+++ src/web/index.rs
@@ -1,6 +1,6 @@
 use crate::db::{Result, Album, Track};
 
-use actix_web::{get, http::header, web, HttpResponse};
+use actix_web::{web, get, http::header, HttpResponse};
 use sqlx::{Pool, Postgres};
 
 markup::define! {
@@ -11,6 +11,7 @@ markup::define! {
                 .albums {
                     @for album in albums {
                         .album {
+                            img[src=format!("/cover/{}", urlencoding::encode(&album.path).to_string()), style="width: 64px;"];
                             h3 { @album.name }
                         }
                     }
@@ -28,4 +29,22 @@ pub async fn page(data: web::Data<Pool<Postgres>>) -> HttpResponse {
     HttpResponse::Ok()
         .insert_header(header::ContentType::html())
         .body(Index { albums }.to_string())
+}
+
+#[get("/cover/{id}")]
+pub async fn cover(data: web::Data<Pool<Postgres>>, path: web::Path<String>) -> HttpResponse {
+    let (mime, data) = match sqlx::query_as::<sqlx::Postgres, (String, Vec<u8>)>("SELECT cover_mime, cover FROM albums WHERE path = $1")
+        .bind(path.into_inner())
+        .fetch_one(data.get_ref()).await {
+            Ok(album) => album,
+            Err(_) => return HttpResponse::NotFound().finish(),
+        };
+
+    HttpResponse::Ok()
+        .insert_header(match mime.as_str() {
+            "png" => header::ContentType::png(),
+            "jpg" => header::ContentType::jpeg(),
+            _ => { unimplemented!() }
+        })
+        .body(data)
 }
