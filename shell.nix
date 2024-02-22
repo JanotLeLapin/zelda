bf76fd6b6eb03e4d57e0a7684a6bc0de55cc2c73
Joseph DALY <joseph300905@gmail.com>
2024-02-22 01:23:36 +0100

    added web ui

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
 { rust-bin
 , rust-analyzer
+, nodejs
 , mkShell
 }: mkShell {
   packages = [
     (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-    rust-analyzer
+    rust-analyzer nodejs
   ];
 }
